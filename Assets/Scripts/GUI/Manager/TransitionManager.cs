ca0dd1ae1bd6bd51c0e801ca25e5ff302f364eb9
Keyboard Destroyer <43577127+kbrddestroyer@users.noreply.github.com>
2024-01-23 15:32:42 +0300

    Fixed MainMenu

--- Assets/Scripts/GUI/Manager/TransitionManager.cs
+++ Assets/Scripts/GUI/Manager/TransitionManager.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class TransitionManager : MonoBehaviour
 {
+    [Header("Transition Manager Required Components")]
+    [SerializeField, SceneObjectsOnly] private Animator transition;
+
+    private IEnumerator asyncSceneLoader(string sceneName)
+    {
+        transition.SetTrigger("transition");
+        yield return new WaitForSeconds(5.0f);
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName);
+        loading.allowSceneActivation = false;
+        while (loading.progress < 0.9f) 
+            yield return null;
+
+        loading.allowSceneActivation = true;
+    }
+
     public void Invoke(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(asyncSceneLoader(sceneName));
     }
 }
